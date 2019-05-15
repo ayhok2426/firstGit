2a72fb0e13ffc1583be5d72ca51fad3198eccba7
tjoa ay hok <ayhok2426@gmail.com>
2019-05-15 08:23:12 +0700

    Initial Commit

---
+++ firstGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firstGit
+//
+//  Created by tjoa ay hok on 15/05/19.
+//  Copyright © 2019 ayhok2426. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
